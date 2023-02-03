6144bfe6e38b74dcfa3269a2e35d5d0ff2383a41
HiBixby <201910789@sangmyung.kr>
2023-02-04 02:25:59 +0900

    ✨ K번째수

---
+++ javascript/정렬/k번째수.js
@@ -0,0 +1,15 @@
+function solution(array, commands) {
+  let answer = [];
+  for (let [i, j, k] of commands) {
+    answer.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]);
+  }
+  return answer;
+}
+
+let array = [1, 5, 2, 6, 3, 7, 4];
+let commands = [
+  [2, 5, 3],
+  [4, 4, 1],
+  [1, 7, 3],
+];
+console.log(solution(array, commands)); //return [5, 6, 3]
